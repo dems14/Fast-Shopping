7e0468e131d2152470d04b45f345980e35ae23d7
dems14 <dems14@gmail.com>
2020-08-11 19:19:56 -0400

    cart component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import data from './data.json'
 import Products from './components/Products';
 import Filter from './components/Filter';
+import Cart from './components/Cart';
 
 
 class App extends React.Component {
@@ -9,10 +10,34 @@ class App extends React.Component {
     super();
     this.state = {
       products : data.products,
-      sort: ""
+      sort: "",
+      cartItems: []
     };
   }
 
+  removeFromCart = (product) => {
+    const cartItems = this.state.cartItems.slice();
+    this.setState({
+      cartItems:  cartItems.filter((x) => x._id !== product._id),
+    });
+  };
+
+  addToCart = (product) => {
+    const cartItems = this.state.cartItems.slice();
+    let alreadyInCart = false
+    cartItems.forEach((item) =>{
+      if(item._id === product._id) {
+        item.count ++;
+        alreadyInCart = true;
+      }
+    });
+
+    if(!alreadyInCart) {
+      cartItems.push({...product, count: 1})
+    }
+
+    this.setState({cartItems});
+  }
   sortProducts = (event) => {
 
     console.log(event.target.value);
@@ -50,10 +75,10 @@ class App extends React.Component {
                 sortProducts={this.sortProducts}
                 >
                 </Filter>
-                <Products products={this.state.products}></Products>
+                <Products products={this.state.products} addToCart={this.addToCart}></Products>
             </div>
             <div className="sidebar">
-                Cart Items
+                <Cart cartItems={this.state.cartItems} removeFromCart={this.removeFromCart}></Cart>
             </div>
           </div>
         </main>
